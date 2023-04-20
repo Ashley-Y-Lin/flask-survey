53692c81888285ad3420ae3f3e0ef670a8e771e2
Graham MacFarquhar <gmacfarquhar57@gmail.com>
2023-04-20 11:32:11 -0700

    1-3

--- app.py
+++ app.py
@@ -7,3 +7,26 @@ app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
+
+responses = []
+
+@app.get("/")
+def start():
+    """renders survey start page"""
+
+    return render_template('survey_start.html')
+
+@app.post('/begin')
+def begin():
+    """handle button to begin survey"""
+
+    return redirect('/questions/0')
+
+@app.get('/questions/<prompt_num>')
+def questions(prompt_num):
+
+    question = survey.questions[int(prompt_num)]
+
+    return render_template('question.html', question = question)
+
+#@app.post('/answer')
